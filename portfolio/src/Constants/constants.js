031a169c554d9184fbc6f5a55c0acfbb59c2ea97
chunkay1 <fabian.hernandez1713@gmail.com>
2023-08-02 19:39:59 -0500

    updated base URL

--- portfolio/src/Constants/constants.js
+++ portfolio/src/Constants/constants.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
 
-export const BASEURL = 'https://fabians-portfolio.onrender.com/';
+export const BASEURL = 'https://fabian-hernandez.onrender.com/';
 
 {/* Use this site as an inspiration - https://brittanychiang.com/#about */}
